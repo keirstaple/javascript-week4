f03a8a40f38b3fcb5d38c6793784240d489bba0f
keir staple <keirstaple@comcast.net>
2016-04-08 10:17:09 -0700

    add new-review handlebars and logic

--- app/components/new-review/component.js
+++ app/components/new-review/component.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewReview: false,
+  actions: {
+    reviewFormShow() {
+      this.set('addNewReview', true);
+    },
+
+    saveReview() {
+      var params = {
+        fullName: this.get('fullName'),
+        date: moment.format('LLL'),
+        text: this.get('text'),
+        restaurant: this.get('restaurant')
+      };
+      this.set('addNewReview', false);
+      this.sendAction('saveReview', params);
+    }
+  }
 });
