7d8791a821cc18ac8df7e944ee51411294b332ab
keir staple <keirstaple@comcast.net>
2016-04-08 14:42:20 -0700

    add computed property to allow the sorting of reviews by votes

--- app/components/restaurant-detail/component.js
+++ app/components/restaurant-detail/component.js
@@ -1,11 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  sortBy: ['voteCount:desc'],
+  sortedReviews: Ember.computed.sort('restaurant.reviews', 'sortBy'),
+
+
   actions: {
     delete(restaurant) {
       if(confirm('Are you sure you want to delete this restaurant? If so, press OK to permanently delete.')) {
         this.sendAction('destroyRestaurant', restaurant);
       }
+    },
+
+    saveReview(params) {
+      this.sendAction('saveReview', params);
+    },
+
+    voteUp(vote) {
+      this.sendAction('voteUp', vote);
+    },
+
+    voteDown(vote) {
+      this.sendAction('voteDown', vote);
     }
   }
 });
