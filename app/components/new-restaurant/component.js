649dd9c0b7614ec3afe36eb210aa4a50251a3d33
keir staple <keirstaple@comcast.net>
2016-04-08 09:59:01 -0700

    add new-restaurant handlebars

--- app/components/new-restaurant/component.js
+++ app/components/new-restaurant/component.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewRestaurant: false,
+  actions: {
+    restaurantFormShow() {
+      this.set('addNewRestaurant', true);
+    },
+
+    save() {
+      var params = {
+        name: this.get('name'),
+        location: this.get('location'),
+        cuisine: this.get('cuisine'),
+        image: this.get('cuisine')
+      };
+      this.set('addNewRestaurant', false);
+      this.sendAction('save', params);
+    }
+  }
 });
