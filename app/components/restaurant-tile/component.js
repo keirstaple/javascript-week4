1dcdbfbd4b09cc297d63b84ba7d2667827992ce5
keir staple <keirstaple@comcast.net>
2016-04-08 09:01:56 -0700

    add restaurant route and logic

--- app/components/restaurant-tile/component.js
+++ app/components/restaurant-tile/component.js
@@ -4,7 +4,7 @@ export default Ember.Component.extend({
   actions: {
     delete(restaurant) {
       if(confirm('Are you sure you want to delete this restaurant? If so, press OK to permanently delete.')) {
-        this.sendAction('destroyRestaurant', restaurant)
+        this.sendAction('destroyRestaurant', restaurant);
       }
     }
   }
---
+++ app/restaurant/route.js
@@ -0,0 +1,29 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model(params) {
+    this.store.findRecord('restaurant', params.restaurant_id);
+  },
+
+  actions: {
+    destroyRestaurant(restaurant) {
+      var review_deletions = restaurant.get('reviews').map(function(review) {
+        return review.destroyRecord();
+      });
+      Ember.RSVP.all(review_deletions).then(function(){
+        return restaurant.destroyRecord();
+      })
+      this.transitionTo('index');
+    },
+
+    saveReview(params) {
+      var newReview = this.store.createRecord('review', params);
+      var restaurant = params.restaurant;
+      restaurant.get('reviews').addObject(newReview);
+      newReview.save().then(function(){
+        return restaurant.save();
+      });
+      this.transitionTo('restaurant', params.restaurant);
+    }
+  }
+});
