7935aa864e516a206c6737fc7d683ed442b7d7e7
keir staple <keirstaple@comcast.net>
2016-04-08 09:19:57 -0700

    add restaurant-detail component and logic

--- app/components/restaurant-tile/component.js
+++ app/components/restaurant-tile/component.js
@@ -3,9 +3,9 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     delete(restaurant) {
-      if(confirm('Are you sure you want to delete this restaurant? If so, press OK to permanently delete.')) {
-        this.sendAction('destroyRestaurant', restaurant);
-      }
+      this.sendAction('destroyRestaurant', restaurant);
+      // if(confirm('Are you sure you want to delete this restaurant? If so, press OK to permanently delete.')) {
+      // }
     }
   }
 });
