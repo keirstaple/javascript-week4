0dd8a06b5946b897f960c30c4ee2565c16f7ceb4
keir staple <keirstaple@comcast.net>
2016-04-08 08:43:34 -0700

    add index route logic

---
+++ app/index/route.js
@@ -0,0 +1,32 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return Ember.RSVP.hash({
+      restaurant: this.store.findAll('restaurant'),
+      review: this.store.findAll('review'),
+      rating: this.store.findAll('rating')
+    })
+  },
+
+  actions: {
+    save(params) {
+      var newRestaurant = this.store.createRecord('restaurant', params);
+      newRestaurant.save();
+      this.transitionTo('index');
+    },
+
+    saveReview(params) {
+      var newReview = this.store.createRecord('review', params);
+      newReview.save();
+      this.transitionTo('restaurant');
+    },
+
+    destroyRestaurant(restaurant) {
+      restaurant.destroyRecord();
+      this.transitionTo('index');
+    }
+
+
+  }
+});
